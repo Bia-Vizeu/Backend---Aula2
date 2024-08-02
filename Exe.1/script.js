072ae8f6e6d8210f38c2194c6b57d4624599f32f
Bia Vizeu <161781032+Bia-Vizeu@users.noreply.github.com>
2024-08-02 16:39:26 -0300

    Add files via upload

---
+++ Exe.1/script.js
@@ -0,0 +1,25 @@
+function cadastrar(){
+var nome = document.getElementById("nome").value
+let n1 = Number(document.getElementById("n1").value);
+let n2 = Number(document.getElementById("n2").value);
+let n3 = Number(document.getElementById("n3").value);
+let media; 
+let situacao;
+var tabela_resultado = document.querySelector("#tabela_resultado");
+
+
+        
+
+
+
+// media = (n1+n2+n3)/3;
+// console.log("A media do", nome,"e de:", media);
+
+// if(media>= 50)(
+//     console.log("Aprovado")
+
+// ) else (
+//     console.log("Reprovado")
+// )
+
+}
